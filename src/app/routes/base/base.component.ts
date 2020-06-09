96ad36587175e1dfc140828099f19701d94957dd
Bas van Rooten <basvanrooten@me.com>
2020-06-09 20:39:25 +0200

    Added logo to dashboard

--- src/app/routes/base/base.component.ts
+++ src/app/routes/base/base.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: "app-base",
     templateUrl: "./base.component.html",
-    styleUrls: ["./base.component.css"],
+    styleUrls: ["./base.component.scss"],
 })
 export class BaseComponent {
     public readonly urlParts = window.location.href;
